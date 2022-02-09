ea9c5144a0dd59f5638a83450c9fc89f4dbfcbe4
Brad Breiten <jonathan@breiten.us>
2022-02-09 12:27:28 -0700

    Test: find and find all functions in MerchantsController

--- spec/requests/api/v1/merchants_request_spec.rb
+++ spec/requests/api/v1/merchants_request_spec.rb
@@ -64,4 +64,71 @@ RSpec.describe 'The Merchants API' do
       end
     end
   end
+
+  describe 'GET /api/v1/merchants/find_all' do
+    let!(:new_merchant_1) { create(:merchant, name: 'Axe Slinging Fun Times') }
+
+    context 'when a full name is passed' do
+      before { get '/api/v1/merchants/find_all', params: {name: 'Axe Slinging Fun Times' } }
+
+      it 'returns a list of merchants with that name' do
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants[:data].count).to eq(1)
+        expect(merchants[:data][0][:attributes][:name]).to eq('Axe Slinging Fun Times')
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when a partial term is passed' do
+      let!(:new_merchant_2) {create(:merchant, name: 'Timers R Us') }
+
+      before { get '/api/v1/merchants/find_all', params: {name: 'time' } }
+
+      it 'returns merchants with the term in their name' do
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants[:data].count).to eq(2)
+        expect(response).to have_http_status(200)
+
+        merchants[:data].each do |merchant|
+          expect(merchant[:attributes][:name].downcase.include?('time')).to be true
+        end
+      end
+    end
+  end
+
+  describe 'GET /api/v1/merchants/find' do
+    let!(:new_merchant_1) { create(:merchant, name: 'Slippery Dave') }
+
+    context 'when a full name is passed' do
+      before { get '/api/v1/merchants/find', params: {name: 'Slippery Dave' } }
+
+      it 'returns a single merchant with that name' do
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants[:data].count).to eq(1)
+        expect(merchants[:data][0][:attributes][:name]).to eq('Slippery Dave')
+        expect(response).to have_http_status(200)
+      end
+    end
+
+    context 'when a partial term is passed' do
+      let!(:new_merchant_2) {create(:merchant, name: 'Slip n Slide') }
+
+      before { get '/api/v1/merchants/find', params: {name: 'SlI' } }
+
+      it 'returns the first merchant that matches that partial search' do
+        merchants = JSON.parse(response.body, symbolize_names: true)
+
+        expect(merchants[:data].count).to eq(1)
+        expect(response).to have_http_status(200)
+        expect(merchants[:data][0][:attributes][:name]).to eq('Slippery Dave')
+
+        merchants[:data].each do |merchant|
+          expect(merchant[:attributes][:name].downcase.include?('sli')).to be true
+        end
+      end
+    end
+  end
 end
