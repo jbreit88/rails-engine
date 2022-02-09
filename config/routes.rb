bce7d1e95b909754f64d75b9d87106e96a4b472d
Brad Breiten <jonathan@breiten.us>
2022-02-08 22:37:10 -0700

    Feat: destroy item and conditionally dependent data

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
       resources :merchants, only: %i[index show]
-      resources :items, only: %i[index show create update]
+      resources :items
     end
   end
 end
